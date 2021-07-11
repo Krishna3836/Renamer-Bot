2090c0679211d204b478a04c1c71010cd86bdeaf
Animesh Verma <85546858+AVBotz-TG@users.noreply.github.com>
2021-07-11 18:12:32 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -10,7 +10,7 @@ class Config(object):
     SESSION_NAME = os.environ.get("SESSION_NAME", "Rename-Bot-0")
     SLEEP_TIME = int(os.environ.get("SLEEP_TIME", 5))
     BOT_OWNER = os.environ.get("BOT_OWNER", 1445283714)
-    CAPTION = "Rename Bot by @{}\n\nMade by @AbirHasan2005"
+    CAPTION = "© By @AVBotz ❤️"
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -100))
     MONGODB_URI = os.environ.get("MONGODB_URI", "")
