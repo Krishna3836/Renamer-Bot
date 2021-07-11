4eca372d2a64608195c8a42534fe5c8ebb511861
Animesh Verma <85546858+AVBotz-TG@users.noreply.github.com>
2021-07-11 16:49:41 +0530

    Update display_progress.py

--- helpers/display_progress.py
+++ helpers/display_progress.py
@@ -32,7 +32,7 @@ async def progress_for_pyrogram(current, total, ud_type, message, start):
         )
         try:
             await message.edit(
-                text="**{}\n\n{}**".format(
+                text="**⛕ {}\n\n{}**".format(
                     ud_type,
                     tmp
                 ),
