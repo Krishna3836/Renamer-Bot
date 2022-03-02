fd90ca13a8899721ff486514ba1b624ebca881a2
Naysa Bots <96517910+Naysabots@users.noreply.github.com>
2022-03-02 14:23:51 +0530

    Update uploader.py

--- helpers/uploader.py
+++ helpers/uploader.py
@@ -15,57 +15,68 @@ from hachoir.metadata import extractMetadata
 from pyrogram import Client
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, Thumbnail
 
-
-async def UploadFile(bot: Client, message: Message, file_path: str, file_size):
+async def UploadVideo(bot: Client, cb: CallbackQuery, file_path: str, width, height, duration, video_thumbnail, file_size):
     try:
-        caption_ = await db.get_caption(message.chat.id)
-        db_thumbnail = await db.get_thumbnail(message.chat.id)
-        file_thumbnail = None
-        if db_thumbnail is not None:
-            file_thumbnail = await bot.download_media(
-                message=db_thumbnail,
-                file_name=f"{Config.DOWNLOAD_PATH}/{str(message.chat.id)}/thumbnail/"
+        sent_ = None
+        if (await db.get_upload_as_doc(cb.from_user.id)) is False:
+            c_time = time.time()
+            sent_ = await bot.send_video(
+                chat_id=cb.message.chat.id,
+                video=file_path,
+                width=width,
+                height=height,
+                duration=duration,
+                thumb=video_thumbnail,
+                caption=Config.CAPTION.format((await bot.get_me()).username) + f"\n\n**File Name:** `{merged_vid_path.rsplit('/', 1)[-1]}`\n**Duration:** `{format_timespan(duration)}`\n**File Size:** `{humanbytes(file_size)}`",
+                progress=progress_for_pyrogram,
+                progress_args=(
+                    "Uploading Video ...",
+                    cb.message,
+                    c_time
+                ),
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [InlineKeyboardButton("Developer - @AbirHasan2005", url="https://t.me/AbirHasan2005")],
+                        [InlineKeyboardButton("Support Group", url="https://t.me/linux_repo"),
+                         InlineKeyboardButton("Bots Channel", url="https://t.me/Discovery_Updates")]
+                    ]
+                )
             )
-            Image.open(file_thumbnail).convert("RGB").save(file_thumbnail)
-            img = Image.open(file_thumbnail)
-            img.resize((100, 100))
-            img.save(file_thumbnail, "JPEG")
-        c_time = time.time()
-        sent_ = await bot.send_document(
-            chat_id=message.chat.id,
-            document=file_path,
-            progress=progress_for_pyrogram,
-            progress_args=(
-                "**📤 Uploading File...**",
-                message,
-                c_time
-            ),
-            force_document=True,
-            thumb=file_thumbnail,
-            caption=((Config.CAPTION.format((await bot.get_me()).username) + f"\n\n**File Name: {file_path.rsplit('/', 1)[-1]}**") if (caption_ is None) else caption_),
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [InlineKeyboardButton("👀 More Amazing Botz 🤖", url="https://t.me/AVBotz/5")]
-                ]
+        else:
+            c_time = time.time()
+            sent_ = await bot.send_document(
+                chat_id=cb.message.chat.id,
+                document=file_path,
+                caption=Config.CAPTION.format((await bot.get_me()).username) + f"\n\n**File Name:** `{merged_vid_path.rsplit('/', 1)[-1]}`\n**Duration:** `{format_timespan(duration)}`\n**File Size:** `{humanbytes(file_size)}`",
+                thumb=video_thumbnail,
+                progress=progress_for_pyrogram,
+                progress_args=(
+                    "Uploading Video ...",
+                    cb.message,
+                    c_time
+                ),
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [InlineKeyboardButton("Developer - @AbirHasan2005", url="https://t.me/AbirHasan2005")],
+                        [InlineKeyboardButton("Support Group", url="https://t.me/linux_repo"),
+                         InlineKeyboardButton("Bots Channel", url="https://t.me/Discovery_Updates")]
+                    ]
+                )
             )
-        )
-        await asyncio.sleep(Config.SLEEP_TIME)
+        await asyncio.sleep(Config.TIME_GAP)
         forward_ = await sent_.forward(chat_id=Config.LOG_CHANNEL)
         await forward_.reply_text(
-            text=f"**User: [{message.chat.first_name}](tg://user?id={str(message.chat.id)})**\n**Username: {message.chat.username}**",
+            text=f"**User:** [{cb.from_user.first_name}](tg://user?id={str(cb.from_user.id)})\n**Username:** `{cb.from_user.username}`\n**UserID:** `{cb.from_user.id}`",
             disable_web_page_preview=True,
             quote=True
         )
     except Exception as err:
+        print(f"Failed to Upload Video!\nError: {err}")
         try:
-            await message.edit(f"**Something Went Wrong... Contact [Here](https://t.me/AVBotz_Support)**")
-            await asyncio.sleep(50)
+            await cb.message.edit(f"Failed to Upload Video!\n**Error:**\n`{err}`")
         except:
-            print(f"**Failed to Upload File!\nError: {err}**")
-    await delete_one(file_path)
-    if Config.ONE_PROCESS_ONLY:
-        await CheckTimeGap(message.chat.id, rm_gap=True)
-    await message.delete(True)
+            pass
+
 
 
 
